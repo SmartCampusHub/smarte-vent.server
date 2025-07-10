1cee6e69b1f35dbc6f8fa40c42a5ae761c3ed33b
Win Nguyen <winnguyen1905.work@gmail.com>
2025-07-10 23:34:35 +0700

    Add AccountSearchCriteria DTO for account search functionality and update import paths in StudentStatisticsController
    - Introduced AccountSearchCriteria to facilitate account search with fields for full name, email, phone, identify code, role, major, and active status.
    - Updated import paths in StudentStatisticsController to reflect the new model structure.
    - Removed OrganizationSearchRequest class as part of code cleanup.

---
+++ src/main/java/com/winnguyen1905/Activity/model/dto/AccountSearchCriteria.java
@@ -0,0 +1,22 @@
+package com.winnguyen1905.activity.model.dto;
+
+import com.winnguyen1905.activity.common.constant.AccountRole;
+import com.winnguyen1905.activity.common.constant.MajorType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AccountSearchCriteria implements AbstractModel {
+  private String fullName;
+  private String email;
+  private String phone;
+  private String identifyCode;
+  private AccountRole role;
+  private MajorType major;
+  private Boolean isActive;
+}
